970ef23c89d6d063086bd6b1f896bb2cdbf91bf3
Bryant Gazcon <bryantgazcon@gmail.com>
2022-12-26 20:42:14 -0800

    updated

--- The_Basics/Exercise_3.rb
+++ The_Basics/Exercise_3.rb
@@ -1,3 +1,5 @@
+#Write a program that uses a hash to store a list of movie 
+#titles with the year they came out
 Movies = {:Titanic => 1997, :ANewHope => 1978, :Inception =>2010, :Tenet => 2020 }
 
 Movies.each {|movie, year| puts year }
---
+++ The_Basics/Exercise_4.rb
@@ -0,0 +1,6 @@
+#Use the dates from the previous example and store them in an array. 
+#Then make your program output the same thing as exercise 3.
+
+movies = [1997, 1978, 2010, 2020]
+puts movies
+
