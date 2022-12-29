412f02d66ce31742cf37d595ede5a615f2409dc0
Bryant Gazcon <bryantgazcon@gmail.com>
2022-12-28 19:43:20 -0800

    updated

---
+++ Flow_Control/Exercise_5.rb
@@ -0,0 +1,11 @@
+def equal_to_four(x)
+  if x == 4
+    puts "yup"
+  else
+    puts "nope"
+end
+
+equal_to_four(5)
+
+#when running code, you get error, why do you get it and how to fix it?
+  # missing end to close off our method, just add another end
