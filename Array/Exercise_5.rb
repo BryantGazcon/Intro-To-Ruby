cc5a721e07c8f137cd0b0796b4f812f7cd2b85e3
Bryant Gazcon <bryantgazcon@gmail.com>
2023-01-01 18:07:40 -0800

    initial commit

---
+++ Array/Exercise_5.rb
@@ -0,0 +1,12 @@
+#What is the value of a, b, and c in the following program?
+
+
+      string = "Welcome to   America!"
+# #ofcharacters 012345678910 11
+a = string[6]
+b = string[11]
+c = string[19]
+
+a = "e"
+b = "A"
+c = nil
