32ad418d7720b9e3e4653809e74b262a63d9efce
Bryant Gazcon <bryantgazcon@gmail.com>
2023-01-01 17:47:40 -0800

    initial commit

---
+++ Array/Exercise_3.rb
@@ -0,0 +1,9 @@
+# How do you return the word "example" from the following array?
+
+
+arr = [["test", "hello", "world"],["example", "mem"]]
+
+#couple ways to return
+arr.last.first
+arr.last[0]
+arr[1][0]
