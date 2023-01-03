4017279af48b9a86d18d53023bdbff6e50ea6e6a
Bryant Gazcon <bryantgazcon@gmail.com>
2023-01-03 15:05:21 -0800

    initial commit

---
+++ Hashes/Exercise_7.rb
@@ -0,0 +1,6 @@
+# If you see this error, what do you suspect is the most likely problem?
+
+NoMethodError: undefined method `keys' for Array
+
+# this error means that there is no method called keys for array objects which
+is in fact true since keys is a hash method
