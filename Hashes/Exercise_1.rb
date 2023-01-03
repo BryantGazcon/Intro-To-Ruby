50d1e847eff66d65e45babbb811ecac675ebe2f9
Bryant Gazcon <bryantgazcon@gmail.com>
2023-01-03 14:24:23 -0800

    initial commit

---
+++ Hashes/Exercise_1.rb
@@ -0,0 +1,13 @@
+# Given a hash of family members, with keys as the title and an array of 
+#names as the values, use Ruby's built-in select method to gather 
+#only siblings' names into a new array.
+
+family = {  uncles: ["bob", "joe", "steve"],
+            sisters: ["jane", "jill", "beth"],
+            brothers: ["frank","rob","david"],
+            aunts: ["mary","sally","susan"]
+          }
+
+immediate_family = family.select {|k,v| (k == :brothers) || (k == :sisters)}
+
+p immediate_family.values.flatten
