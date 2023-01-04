48fe765f554941e3cd11ca36ff2b651676eb23fd
Bryant Gazcon <bryantgazcon@gmail.com>
2023-01-03 16:22:02 -0800

    initial commit

---
+++ More_Stuff/Exercise_3.rb
@@ -0,0 +1,10 @@
+=begin
+
+What is expection handling and what problem does it solve?
+
+- Exception Handling is the process in which errros are dealt with in a manageable
+and predicatable way. When programs interact with the real word many issues
+can arise, therefore through the use of reserved word rescue we can implement
+it into our code to provide an alternative in the event of an operation faliure.
+
+=end
