5362554b97448d62b385ab489414da606f5a6f19
Bryant Gazcon <bryantgazcon@gmail.com>
2023-01-04 17:35:38 -0800

    initial commit

---
+++ Exercises/Exercise_14.rb
@@ -0,0 +1,8 @@
+array = ['white snow', 'winter wonderland', 'melting ice',
+     'slippery sidewalk', 'salted roads', 'white trees'] 
+
+array_new = array.map {|string| string.split }
+
+array_new = array_new.flatten
+
+p array_new
