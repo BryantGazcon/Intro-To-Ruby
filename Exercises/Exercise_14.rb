85105fa24f96c9145a9a08e865e67b365aaa0c77
Bryant Gazcon <bryantgazcon@gmail.com>
2023-01-04 19:39:41 -0800

    updated

--- Exercises/Exercise_14.rb
+++ Exercises/Exercise_14.rb
@@ -1,3 +1,5 @@
+# Split array so that each string is an individual element
+
 array = ['white snow', 'winter wonderland', 'melting ice',
      'slippery sidewalk', 'salted roads', 'white trees'] 
 
@@ -6,3 +8,4 @@ array_new = array.map {|string| string.split }
 array_new = array_new.flatten
 
 p array_new
+
