ba75b3496eff63e9027f938595bd3a6228948716
Bryant Gazcon <bryantgazcon@gmail.com>
2023-01-04 16:09:10 -0800

    initial commit

---
+++ Exercises/Exercise_7.rb
@@ -0,0 +1,11 @@
+=begin
+What's the major difference between an Array and a Hash?
+
+The major difference between an array and a hash is that hashes sometimes
+nicknamed dictionaries have two parts, a key and it's value. By storing a key
+along with its value in the hash you are able to call upon that key 
+anywhere within your program and recieve the initial value that was associated
+with that key. On the other hand arrays are simply used to store data which can
+range anywhere from integers to strings to floats etc.
+
+=end
